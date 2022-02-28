7bc2e71c359a9d3cb222c2882ea917f911310655
unknown <phucuong200297@gmail.com>
2022-02-28 13:13:34 +0700

    upfile

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000;
+const { request } = require('https')
+const port = process.env.PORT ||3000;
+const fs = require('fs');
 
 
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
+
+app.use(express.static('public'))
+
+app.get('/loc', (request, response) => {
+    console.log(request.params)
+    var url_page = request.query;
+    var string = JSON.stringify(url_page);
+    var objectValue = JSON.parse(string);
+    var get_authorization_code = objectValue['code'];
+  
+    console.log("Authorization Code: " + get_authorization_code);
+  })
 
 
 app.listen(port,()=>{
